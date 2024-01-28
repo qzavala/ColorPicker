b2bfa61d1a27ee4f6cbfa03997c44e86f62b1ccb
quiryat zavala <qzavala2014@gmail.com>
2024-01-28 11:28:30 -0800

    added colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 // Write your Color component here
 
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
+
 const App = () => {
   return (
     <div id="container">
@@ -7,7 +11,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+       <Color color="violet"/>
+  <Color color="red"/>
+  <Color color="blue" /></div>
+      
     </div>
   );
 };
