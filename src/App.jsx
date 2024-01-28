0694bbe8e52c25bf8ae04072ea5047d3535abafe
quiryat zavala <qzavala2014@gmail.com>
2024-01-28 11:38:53 -0800

    selected colors now show on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 // Write your Color component here
+import { useState } from "react";
 
-const Color = (props) => {
-  return <div className={props.color}></div>
+const Color = ({color, setSelectedColor}) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
 }
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-       <Color color="violet"/>
-  <Color color="red"/>
-  <Color color="blue" /></div>
-      
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
